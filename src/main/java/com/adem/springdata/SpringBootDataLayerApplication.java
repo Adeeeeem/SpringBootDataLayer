859683f8250cc1aa7bec7607bc2276cabb736449
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-02-19 23:53:14 +0100

    Initial commit

---
+++ src/main/java/com/adem/springdata/SpringBootDataLayerApplication.java
@@ -0,0 +1,13 @@
+package com.adem.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adem/springdata/SpringBootDataLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adem.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
