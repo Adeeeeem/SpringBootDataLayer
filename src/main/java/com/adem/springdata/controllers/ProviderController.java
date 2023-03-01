2ecb207e41e1c24a3c4f3bed8e7863abc44e8067
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-03-01 15:36:05 +0100

    Updated the controller with the ProviderService

--- src/main/java/com/adem/springdata/controllers/ProviderController.java
+++ src/main/java/com/adem/springdata/controllers/ProviderController.java
@@ -21,6 +21,7 @@ import com.adem.springdata.repositories.ProviderRepository;
 public class ProviderController
 {
 	private final ProviderRepository providerRepository;
+	private final ProviderService providerService;
 
 	public ProviderController()
 	{
@@ -28,84 +29,121 @@ public class ProviderController
 	}
 
 	@Autowired
-	public ProviderController(ProviderRepository providerRepository)
+	public ProviderController(ProviderRepository providerRepository, ProviderService providerService)
 	{
 		this.providerRepository = providerRepository;
+		this.providerService = providerService;
 	}
 
-    @GetMapping("list")
-    //@ResponseBody
-    public String listProviders(Model model)
-    {
-        List <Provider> lp = (List <Provider>) providerRepository.findAll();
-        
-        if(lp.size() == 0)
-        	lp = null;
+	@GetMapping("list")
+	//@ResponseBody
+	public String listProviders(Model model)
+	{
+		// Without Service
+		//List <Provider> lp = (List <Provider>) providerRepository.findAll();
+		
+		//if(lp.size() == 0)
+			//lp = null;
 
-        model.addAttribute("providers", lp);
-        
-        return "provider/listProviders";
-    }
-    
-    @GetMapping("add")
-    public String showAddProviderForm(Model model)
-    {
-    	Provider provider = new Provider();
-    	model.addAttribute("provider", provider);
-        return "provider/addProvider";
-    }
-    
-    @PostMapping("add")
-    public String addProvider(@Valid Provider provider, BindingResult result)
-    {
-        if (result.hasErrors())
-            return "provider/addProvider";
-        
-        if(provider.getName() == "")
-        	provider.setName(null);
-        
-        if(provider.getEmail() == "")
-        	provider.setEmail(null);
-        
-        if(provider.getAddress() == "")
-        	provider.setAddress(null);
-        
-        providerRepository.save(provider);
+		// With Service
+		List <Provider> lp = this.providerService.getAllProviders();
 
-        return "redirect:list";
-    }
- 
-    @GetMapping("delete/{id}")
-    public String deleteProvider(@PathVariable("id") long id, Model model)
-    {
-    	Provider provider = providerRepository.findById(id)
-    			.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + id));        
-        providerRepository.delete(provider);
-        
-        /*model.addAttribute("providers", providerRepository.findAll());
-        return "provider/listProviders";*/
-        return "redirect:../list";
-    }
-    
-    @GetMapping("edit/{id}")
-    public String showProviderFormToUpdate(@PathVariable("id") long id, Model model)
-    {
-    	Provider provider = providerRepository.findById(id)
-    			.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + id));
-        
-        model.addAttribute("provider", provider);
-        
-        return "provider/updateProvider";
-    }
+		model.addAttribute("providers", lp);
+		
+		return "provider/listProviders";
+	}
+	
+	@GetMapping("add")
+	public String showAddProviderForm(Model model)
+	{
+		Provider provider = new Provider();
+		model.addAttribute("provider", provider);
+		return "provider/addProvider";
+	}
+	
+	@PostMapping("add")
+	public String addProvider(@Valid Provider provider, BindingResult result)
+	{
+		if(provider.getName() == "")
+			provider.setName(null);
 
-    @PostMapping("update")
-    public String updateProvider(@Valid Provider provider, BindingResult result, Model model)
-    {
-    	if (result.hasErrors())
-    		return "provider/updateProvider";
+		if(provider.getEmail() == "")
+			provider.setEmail(null);
+		
+		if(provider.getAddress() == "")
+			provider.setAddress(null);
 
-    	providerRepository.save(provider);
+		if (result.hasErrors())
+			return "provider/addProvider";
 
-    	return"redirect:list";
-    }
+		// Without Service
+		//providerRepository.save(provider);
+
+		// With Service
+		this.providerService.persistProvider(provider);
+
+		return "redirect:list";
+	}
+
+	@GetMapping("delete/{id}")
+	public String deleteProvider(@PathVariable("id") long id, Model model)
+	{
+		Provider provider = providerRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + id));
+
+		providerRepository.delete(provider);
+		
+		/*model.addAttribute("providers", providerRepository.findAll());
+		return "provider/listProviders";*/
+
+		return "redirect:../list";
+	}
+	
+	@GetMapping("edit/{id}")
+	public String showProviderFormToUpdate(@PathVariable("id") long id, Model model)
+	{
+		Provider provider = providerRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + id));
+		
+		model.addAttribute("provider", provider);
+		
+		return "provider/updateProvider";
+	}
+
+	@PostMapping("update")
+	public String updateProvider(@Valid Provider provider, BindingResult result, Model model)
+	{
+		if(provider.getName() == "")
+			provider.setName(null);
+
+		if(provider.getEmail() == "")
+			provider.setEmail(null);
+		
+		if(provider.getAddress() == "")
+			provider.setAddress(null);
+
+		if (result.hasErrors())
+			return "provider/updateProvider";
+
+		providerRepository.save(provider);
+
+		return"redirect:list";
+	}
+
+	@GetMapping("show/{id}")
+	public String showProvider(@PathVariable("id") long id, Model model)
+	{
+		Provider provider = providerRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + id));
+
+		List <Article> articles = providerRepository.findArticlesByProvider(id);
+
+		for (Article a : articles)
+			System.out.println("Article = " + a.getLabel());
+
+		model.addAttribute("articles", articles);
+		model.addAttribute("provider", provider);
+
+		return "provider/showProvider";
+	}
 }
