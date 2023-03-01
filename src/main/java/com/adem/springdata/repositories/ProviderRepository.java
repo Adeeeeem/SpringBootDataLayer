6514774d09c2ff3182033cabc8f42d17351fc4ac
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-03-01 15:44:28 +0100

    Add the @Query for Articles

--- src/main/java/com/adem/springdata/repositories/ProviderRepository.java
+++ src/main/java/com/adem/springdata/repositories/ProviderRepository.java
@@ -1,12 +1,17 @@
 package com.adem.springdata.repositories;
 
+import com.adem.springdata.entities.Article;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.adem.springdata.entities.Provider;
 
+import java.util.List;
+
 @Repository
 public interface ProviderRepository extends CrudRepository <Provider, Long>
 {
-
+	@Query("FROM Article a WHERE a.provider.id = ?1")
+    List<Article> findArticlesByProvider(long id);
 }
