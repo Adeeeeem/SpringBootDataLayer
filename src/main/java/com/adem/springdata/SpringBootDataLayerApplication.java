7b94678336b52d7640c637d59258c1fa998d3854
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-03-01 11:45:55 +0100

    set path to create to upload directory

--- src/main/java/com/adem/springdata/SpringBootDataLayerApplication.java
+++ src/main/java/com/adem/springdata/SpringBootDataLayerApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 //import com.adem.springdata.entities.Provider;
-//import com.adem.springdata.repositories.ProviderRepository;
+import com.adem.springdata.controllers.ArticleController;
 
 @EnableAutoConfiguration
 
@@ -15,7 +21,17 @@ public class SpringBootDataLayerApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(SpringBootDataLayerApplication.class, args);
-		
+
+		new File(ArticleController.uploadDirectory).mkdir();
+
+		Path path = Paths.get(ArticleController.uploadDirectory);
+
+		try
+		{
+			Files.createDirectory(path);
+		}
+		catch(IOException e) {}
+
 		//Provider P1 = new Provider();
 		//Provider P2 = new Provider();
 	}
