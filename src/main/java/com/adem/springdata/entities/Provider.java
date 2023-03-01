a57a63958b3b142478c28e56d4a11f2663629538
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-03-01 15:37:25 +0100

    Add articles Column

--- src/main/java/com/adem/springdata/entities/Provider.java
+++ src/main/java/com/adem/springdata/entities/Provider.java
@@ -1,11 +1,8 @@
 package com.adem.springdata.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Entity
 public class Provider
@@ -14,83 +11,97 @@ public class Provider
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
-    @NotBlank(message = "Name is mandatory")
-    @Column(name = "name", nullable = false)
-    private String name;
+	@NotBlank(message = "Name is mandatory")
+	@Column(name = "name", nullable = false)
+	private String name;
 
-    @NotBlank(message = "Email is mandatory")
-    @Column(name = "email", nullable = false)
-    private String email;
+	@NotBlank(message = "Email is mandatory")
+	@Column(name = "email", nullable = false)
+	private String email;
 
-    @NotBlank(message = "Address is mandatory")
-    @Column(name = "address", nullable = false)
-    private String address;
+	@NotBlank(message = "Address is mandatory")
+	@Column(name = "address", nullable = false)
+	private String address;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "provider")
+	private List <Article> articles;
 
 	public Provider()
-    {
-    	System.out.println("Hello From Constructor Block");
-    }
-    
-    public Provider(String name, String address, String email)
-    {
-        this.name = name;
-        this.address = address;
-        this.email = email;
-    }
+	{
+		System.out.println("Hello From Constructor Block");
+	}
+	
+	public Provider(String name, String address, String email)
+	{
+		this.name = name;
+		this.address = address;
+		this.email = email;
+	}
 
-    public long getId()
-    {
-        return id;
-    }
+	public long getId()
+	{
+		return id;
+	}
 
-    public void setId(long id)
-    {
-        this.id = id;
-    }
+	public void setId(long id)
+	{
+		this.id = id;
+	}
 
-    public String getName()
-    {
-        return name;
-    }
+	public String getName()
+	{
+		return name;
+	}
 
-    public void setName(String name)
-    {
-        this.name = name;
-    }
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
-    public String getAddress()
-    {
-        return address;
-    }
+	public String getEmail()
+	{
+		return email;
+	}
 
-    public void setAddress(String address)
-    {
-        this.address = address;
-    }
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
 
-    public String getEmail()
-    {
-        return email;
-    }
+	public String getAddress()
+	{
+		return address;
+	}
 
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-    
-    // Instance Block (first block to run)
-    {
-    	System.out.println("Hello From Instance Block");
-    }
-    // Class Block (first block to run and it runs only one time)
-    static
-    {
-    	System.out.println("Hello From Class Block");
-    }
-    
-    @Override
-   	public String toString()
-    {
-   		return "Provider [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + "]";
-   	}
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+
+	public List<Article> getArticles()
+	{
+		return articles;
+	}
+
+	public void setArticles(List<Article> articles)
+	{
+		this.articles = articles;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Provider [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + "]";
+	}
+	
+	// Instance Block (first block to run)
+	{
+		System.out.println("Hello From Instance Block");
+	}
+
+	// Class Block (first block to run and it runs only one time)
+	static
+	{
+		System.out.println("Hello From Class Block");
+	}
 }
