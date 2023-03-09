82c675ee08aee4ed5762930bd2508c2f53268abd
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-03-09 23:43:16 +0100

    Create Role & User Repositories

---
+++ src/main/java/com/adem/springdata/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.adem.springdata.repositories;
+
+import com.adem.springdata.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer>
+{
+    Role findByEmail(String name);
+}
