e5ff4aa8b46891d8414d02bdff427f4248b95464
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-03-10 13:39:57 +0100

    UserService with BCryptPasswordEncoder Bean

---
+++ src/main/java/com/adem/springdata/services/UserService.java
@@ -0,0 +1,45 @@
+package com.adem.springdata.services;
+
+import com.adem.springdata.entities.Role;
+import com.adem.springdata.entities.User;
+import com.adem.springdata.repositories.RoleRepository;
+import com.adem.springdata.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service("userService")
+public class UserService
+{
+	private UserRepository userRepository;
+	private RoleRepository roleRepository;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder)
+	{
+		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+	public User findUserByEmail(String email)
+	{
+		return userRepository.findByEmail(email);
+	}
+
+	public void saveUser(User user)
+	{
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setActive(0);
+
+		Role userRole = roleRepository.findByName("USER");
+
+		user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+
+		userRepository.save(user);
+	}
+}
