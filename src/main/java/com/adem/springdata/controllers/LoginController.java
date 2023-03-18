16f81c057d3c025836ccf06a6a09f2940bdf4107
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-03-18 22:59:29 +0100

    Create a new Login Controller

--- src/main/java/com/adem/springdata/controllers/LoginController.java
+++ src/main/java/com/adem/springdata/controllers/LoginController.java
@@ -3,35 +3,105 @@ package com.adem.springdata.controllers;
 import com.adem.springdata.entities.User;
 import com.adem.springdata.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/user")
 public class LoginController
 {
-	UserService userService;
-
 	@Autowired
-	public LoginController(UserService userService)
+	private UserService userService;
+
+	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+	public ModelAndView login()
 	{
-		this.userService = userService;
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+
+		return modelAndView;
 	}
 
-	@RequestMapping("/add")
-	@ResponseBody
-	public String addUser()
+	@RequestMapping(value={"/home"}, method = RequestMethod.GET)
+	public ModelAndView accueil()
 	{
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home");
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		System.out.println(user.getEmail() + " " + user.getFirstName() + " " + user.getLastName());
+
+		return modelAndView;
+	}
+
+	@RequestMapping(value="/registration", method = RequestMethod.GET)
+	public ModelAndView registration()
+	{
+		ModelAndView modelAndView = new ModelAndView();
 		User user = new User();
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("registration");
 
-		user.setEmail("MohamedAdemBenMoussa@Gmail.com");
-		user.setPassword("Domdom");
-		user.setFirstName("Mohamed Adem");
-		user.setLastName("BEN MOUSSA");
+		return modelAndView;
+	}
 
-		this.userService.saveUser(user);
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult)
+	{
+		ModelAndView modelAndView = new ModelAndView();
+		User userExists = userService.findUserByEmail(user.getEmail());
 
-		return "Added";
+		if (userExists != null)
+		{
+			bindingResult
+					.rejectValue("email", "error.user", "There is already a user registered with the email provided");
+		}
+
+		if (bindingResult.hasErrors())
+		{
+			modelAndView.setViewName("registration");
+		}
+		else
+		{
+			userService.saveUser(user);
+			modelAndView.addObject("successMessage", "User has been registered successfully");
+			modelAndView.addObject("user", new User());
+			modelAndView.setViewName("registration");
+		}
+
+		return modelAndView;
+	}
+
+	/* @RequestMapping(value="/admin/home", method = RequestMethod.GET)
+	public ModelAndView home()
+	{
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+		modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+		modelAndView.setViewName("admin/home");
+
+		return modelAndView;
+	}*/
+
+	@GetMapping("/403")
+	public String reddirectError403()
+	{
+		return "redirect:../acessDenied";
+	}
+
+	@GetMapping("/acessDenied")
+	public String error403()
+	{
+		return "/error/403";
 	}
 }
---
+++ src/main/java/com/adem/springdata/controllers/LoginTestController.java
@@ -0,0 +1,37 @@
+package com.adem.springdata.controllers;
+
+import com.adem.springdata.entities.User;
+import com.adem.springdata.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class LoginTestController
+{
+    UserService userService;
+
+    @Autowired
+    public LoginTestController(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/add")
+    @ResponseBody
+    public String addUser()
+    {
+        User user = new User();
+
+        user.setEmail("MohamedAdemBenMoussa@Gmail.com");
+        user.setPassword("Domdom");
+        user.setFirstName("Mohamed Adem");
+        user.setLastName("BEN MOUSSA");
+
+        this.userService.saveUser(user);
+
+        return "Added";
+    }
+}
